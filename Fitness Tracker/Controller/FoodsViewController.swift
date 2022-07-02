37a0d22f4e9fb90cd6fec6e956d5c36be0cbfe39
Petar Iliev <petariliev2002@gmail.com>
2022-07-01 20:37:31 -0700

    Added foods view controller

---
+++ Fitness Tracker/Controller/FoodsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FoodsViewController.swift
+//  Fitness Tracker
+//
+//  Created by Petar Iliev on 1.7.22.
+//
+
+import UIKit
+
+class FoodsViewController : UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
