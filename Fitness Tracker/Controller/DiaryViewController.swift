7120b287b055d196df277e080da7f0a09ed69a00
Petar Iliev <petariliev2002@gmail.com>
2022-07-01 19:40:59 -0700

    Added diary and settings page UI

--- Fitness Tracker/Controller/DiaryViewController.swift
+++ Fitness Tracker/Controller/DiaryViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class DiaryViewController : UIViewController {
     
+    @IBOutlet weak var addMealButton: UIButton!
+    @IBOutlet weak var addWeightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addMealButton.layer.cornerRadius = 10.0
+        addWeightButton.layer.cornerRadius = 10.0
     }
 }
--- Fitness Tracker/Controller/SettingsViewController.swift
+++ Fitness Tracker/Controller/SettingsViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class SettingsViewController : UIViewController {
     
+    @IBOutlet weak var resetInformationButton: UIButton!
+    @IBOutlet weak var resetMealsButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.isHidden = true
+        resetInformationButton.layer.cornerRadius = 10.0
+        resetMealsButton.layer.cornerRadius = 10.0
     }
     
 }
